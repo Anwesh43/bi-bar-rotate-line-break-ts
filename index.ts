96d983a844bb4ac2bf1647e488bd7e5033a66b48
Anwesh43 <anweshthecool0@gmail.com>
2021-05-21 22:47:50 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -29,3 +29,46 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawBiBarRotateLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const barW : number = Math.min(w, h) / barSizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const lSize : number = size * 0.5 * sf1 
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(sf3 * Math.PI / 2)
+        DrawingUtil.drawLine(context, -lSize, 0, lSize, 0)
+        context.save()
+        context.translate(0, -(w / 2 - size) * sf4)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            context.fillRect(size / 2 - barW / 2, -size * sf2, barW, size * sf2)
+            context.restore()
+        }
+        context.restore()
+        context.restore()
+    }
+
+    static drawBRLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawBiBarRotateLine(context, scale)
+    }
+}
