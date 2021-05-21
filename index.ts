ea6be185afe262d51ff4193909524ac56d29cd1e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-21 23:49:25 +0530

    adding BBRLNode class

--- index.ts
+++ index.ts
@@ -147,3 +147,45 @@ class Animator {
         }
     }
 }
+
+class BBRLNode {
+
+    next : BBRLNode 
+    prev : BBRLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BBRLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBiBarRotateLine(context, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BBRLNode {
+        var curr : BBRLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
